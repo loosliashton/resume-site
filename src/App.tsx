fa585761b168f4d65a6f8dfad37ebc50d573690d
loosliashton <loosliashton@gmail.com>
2023-02-28 10:31:03 -0700

    Completed links

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { IconContext } from "react-icons";
+import {
+  BsEnvelopeFill,
+  BsFileEarmarkPersonFill,
+  BsGithub,
+  BsLinkedin,
+  BsYoutube,
+} from "react-icons/bs";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="name">Ashton Loosli</p>
+        <div className="links">
+          <IconContext.Provider value={{ size: "24px", color: "#363636" }}>
+            <a href="https://bit.ly/AshtonLoosliResume" title="Resume">
+              <BsFileEarmarkPersonFill />
+            </a>
+            <a href="https://www.linkedin.com/in/ashtonloosli/" title="LinkedIn">
+              <BsLinkedin />
+            </a>
+            <a href="https://github.com/loosliashton" title="GitHub">
+              <BsGithub />
+            </a>
+            <a href="https://www.youtube.com/ashtonloosli" title="YouTube">
+              <BsYoutube />
+            </a>
+            <a href="mailto:loosliashton@gmail.com" title="Email">
+              <BsEnvelopeFill />
+            </a>
+          </IconContext.Provider>
+        </div>
       </header>
     </div>
   );
