ee25b395bc3ceb3cc9c75825b0644b5f2e9b1331
loosliashton <loosliashton@gmail.com>
2023-02-28 19:33:47 -0700

    Reverting to homemade accordion

--- src/Resume/Resume.tsx
+++ src/Resume/Resume.tsx
@@ -1,27 +1,26 @@
-import { Accordion, AccordionDetails } from "@mui/material";
-import AccordionSummary from "@mui/material/AccordionSummary";
+import { useState } from "react";
 import "./Resume.css";
 
 export function Resume() {
+  let [education, setEducation] = useState(false);
 
   return (
     <div className="resume">
       <div className="headerText">Resume</div>
       <h1>Education</h1>
       <div>
-        <Accordion>
-          <AccordionSummary>
-            <h2>University of Utah</h2>
-          </AccordionSummary>
-        </Accordion>
-        {/* <Accordion alwaysOpen>
-          <Accordion.Item eventKey="Education">
-            <Accordion.Header className="accordionHeader">University of Utah</Accordion.Header>
-            <Accordion.Body>
-              <p>Minor in Business, 3.7 GPA, Dean’s List (7 Semesters)</p>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion> */}
+        <div
+          className="accordionTitle"
+          onClick={() => setEducation(!education)}
+        >
+          <h2>University of Utah</h2>
+          <div className="expandIcon">{education ? "-" : "+"}</div>
+        </div>
+        {education && (
+          <div>
+            <p>Minor in Business, 3.7 GPA, Dean’s List (7 Semesters)</p>
+          </div>
+        )}
       </div>
     </div>
   );
