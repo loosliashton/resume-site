ebb4ac8d2347909afacbd9b5f59972a2ebeca7a8
loosliashton <loosliashton@gmail.com>
2023-02-28 18:34:13 -0700

    Starting accordion implementation

--- src/Resume/Resume.tsx
+++ src/Resume/Resume.tsx
@@ -1,26 +1,21 @@
-import { useState } from "react";
+import { Accordion } from "react-bootstrap";
 import "./Resume.css";
 
 export function Resume() {
-  const [education, setEducation] = useState(false);
 
   return (
     <div className="resume">
       <div className="headerText">Resume</div>
       <h1>Education</h1>
       <div>
-        <div
-          className="accordionTitle"
-          onClick={() => setEducation(!education)}
-        >
-          <h2>University of Utah</h2>
-          <div className="expandIcon">{education ? "-" : "+"}</div>
-        </div>
-        {education && (
-          <div>
-            <p>Minor in Business, 3.7 GPA, Dean’s List (7 Semesters)</p>
-          </div>
-        )}
+        <Accordion>
+          <Accordion.Item eventKey="0">
+            <Accordion.Header>University of Utah</Accordion.Header>
+            <Accordion.Body>
+              <p>Minor in Business, 3.7 GPA, Dean’s List (7 Semesters)</p>
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
       </div>
     </div>
   );
